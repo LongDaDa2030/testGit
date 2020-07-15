978324ccfffdd05774255addc309a9325aa4d7bf
long_glb <longdada_pro@atguigu.com>
2020-07-15 15:06:30 +0800

    hot_fix

--- src/main/java/aom/atguigu/git/happy.java
+++ src/main/java/aom/atguigu/git/happy.java
@@ -4,5 +4,6 @@ public class happy {
 	public static void main(String[] args) {
 		System.out.println("test commit 222");
 		System.out.println("test commit 111");
+		System.out.println("hot_fix");
 	}
 }
